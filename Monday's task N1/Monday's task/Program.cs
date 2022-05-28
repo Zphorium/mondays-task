0eba8ebd45932b58a51811661afd6a14754061b9
Zphorium <106176680+Zphorium@users.noreply.github.com>
2022-05-28 22:54:00 +0400

    Update Program.cs

--- Monday's task N1/Monday's task/Program.cs
+++ Monday's task N1/Monday's task/Program.cs
@@ -3,13 +3,13 @@ string generalvalueAzn = "Ümumi mebleğ (AZN)";
 int valueManat = 10000 ;
 
 string generalvalueUsd = "ABŞ dolları (USD)";
-float valueUsd =(float)(valueManat / 1.7);
+float valueUsd =(float)(valueManat / 1.7);  //1.7Man =1Usd that is why the valueManat is divided into 1.7
 
 string generalvalueRub = "Rusiya rublu (RUB)";
-float valueRub = (float) (valueManat / 0.0265) ;
+float valueRub = (float) (valueManat / 0.0265) ; //0.0265Man =1Rub that is why the valueManat is divided into 1.7
 
 string generalvalueEur = "Avro (EUR)";
-float valueEUR = (float)(valueManat / 1.8282);
+float valueEUR = (float)(valueManat / 1.8282); //1.8282Man =1Usd that is why the valueManat is divided into 1.7
 
 Console.WriteLine(generalvalueAzn);
 Console.WriteLine(valueManat);

f7da0d7ce84e8b289be30d1efeb993bbc585e3e8
Zphorium <zulfugarsm@code.edu.az>
2022-05-28 22:39:20 +0400

    Task 2 added to Repo

---
+++ Monday's task N2/Monday's task N2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+string nameBudget = "Büdce :";
+double yBudgetAzerbaijan = 20000000000;
+
+Console.WriteLine(nameBudget);
+Console.WriteLine(yBudgetAzerbaijan);
+
+string nameBudgetMudafiyeN = "Müdafie Nazirliyine :";
+double yBudgetMudafiyeN = (yBudgetAzerbaijan * 20)/ 100 ;
+Console.WriteLine(nameBudgetMudafiyeN);
+Console.WriteLine(yBudgetMudafiyeN);
+
+string nameBudgetYuksekT = "Yüksek Texnologiyalar Nazirliyine :";
+double yBudgetYüksəkTexn = (yBudgetAzerbaijan * 10) / 100;
+Console.WriteLine(nameBudgetYuksekT);
+Console.WriteLine(yBudgetYüksəkTexn);
+
+
+string nameBudgetLastbudget = "Yerde qalan büdce :";
+double lastBudgetAzerbaijan = yBudgetAzerbaijan - (yBudgetMudafiyeN + yBudgetYüksəkTexn);
+Console.WriteLine(nameBudgetLastbudget);
+Console.WriteLine(lastBudgetAzerbaijan);
+

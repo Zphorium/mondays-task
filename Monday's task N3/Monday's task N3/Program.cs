8865835bff2a41466f63b1192fa527b4cc39de4f
Zphorium <zulfugarsm@code.edu.az>
2022-05-28 22:43:17 +0400

    Task 3 added to Repo

---
+++ Monday's task N3/Monday's task N3/Program.cs
@@ -0,0 +1,82 @@
+﻿
+string brand = "Firma:";
+string brandName1 = "Adidas";
+
+string nameSnumber = "Ayaqqabı sayı : ";
+int numberShoes1 = 4;
+
+string nameValue1 = "Birinin qiymeti ($) :";
+int valueShoes1 = 200;
+
+string namevalueAll = "Hamısının birlikde qiymeti ($) :";
+int valueAll1 = valueShoes1 * numberShoes1;
+
+Console.WriteLine(brand);
+Console.WriteLine(brandName1);
+
+Console.WriteLine(nameSnumber);
+Console.WriteLine(numberShoes1);
+
+Console.WriteLine(nameValue1);
+Console.WriteLine(valueShoes1);
+
+Console.WriteLine(namevalueAll);
+Console.WriteLine(valueAll1);
+
+
+string brandName2 = "Gucci";
+
+int numberShoes2 = 2;
+
+int valueShoes2 = 1000;
+
+int valueAll2 = valueShoes2 * numberShoes2;
+
+Console.WriteLine(brand);
+Console.WriteLine(brandName2);
+
+Console.WriteLine(nameSnumber);
+Console.WriteLine(numberShoes2);
+
+Console.WriteLine(nameValue1);
+Console.WriteLine(valueShoes2);
+
+Console.WriteLine(namevalueAll);
+Console.WriteLine(valueAll2);
+
+string brandName3 = "Nike";
+
+int numberShoes3 = 1;
+
+int valueShoes3 = 150;
+
+int valueAll3 = valueShoes3 * numberShoes3;
+
+Console.WriteLine(brand);
+Console.WriteLine(brandName3);
+
+Console.WriteLine(nameSnumber);
+Console.WriteLine(numberShoes3);
+
+Console.WriteLine(nameValue1);
+Console.WriteLine(valueShoes3);
+
+Console.WriteLine(namevalueAll);
+Console.WriteLine(valueAll3);
+
+string nameValueAll = "yekun xərclənən məbləğ :";
+int valueAll = valueAll1 + valueAll2 + valueAll3;
+Console.WriteLine(namevalueAll);
+Console.WriteLine(valueAll);
+
+
+
+
+
+
+
+
+
+
+
+

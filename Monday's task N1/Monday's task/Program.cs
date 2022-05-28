60abb8e68e8d7546ad07a5ca6a66beafec62f94c
Zphorium <106176680+Zphorium@users.noreply.github.com>
2022-05-28 23:00:49 +0400

    Update Program.cs

--- Monday's task N1/Monday's task/Program.cs
+++ Monday's task N1/Monday's task/Program.cs
@@ -4,15 +4,15 @@ int valueManat = 10000 ;
 
 string generalvalueUsd = "ABŞ dolları (USD)";
 float valueUsd =(float)(valueManat / 1.7);  //1.7Man =1Usd that is why the valueManat is divided into 1.7
-//if exchange rate chanege just chane number 
+//if exchange rate chanege just change number 
 
 string generalvalueRub = "Rusiya rublu (RUB)";
 float valueRub = (float) (valueManat / 0.0265) ; //0.0265Man =1Rub that is why the valueManat is divided into 0.0265
-//if exchange rate chanege just chane number 
+//if exchange rate chanege just change number 
 
 string generalvalueEur = "Avro (EUR)";
 float valueEUR = (float)(valueManat / 1.8282); //1.8282Man =1Usd that is why the valueManat is divided into 1.8282
-//if exchange rate chanege just chane number 
+//if exchange rate chanege just change number 
 
 Console.WriteLine(generalvalueAzn);
 Console.WriteLine(valueManat);
